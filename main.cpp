e5ecc7f59f205eb33886f39c1940e888d506ac99
AnnyBeatriz10 <75858073+AnnyBeatriz10@users.noreply.github.com>
2022-01-25 11:31:51 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//#include <iostream> importar biblioteca básica
+
+//using namespace std;  organização de pastas (conteudo das bibliotecas)
+
+//int função que retorna um innteiro
+
+//void retorno nulo, a função não retorna nada
+
+//main função principal do programa
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Olá!\n Pessoas\n";
+    system("pause");
+    //printf comando do C para escrever na tela
+
+    return 0;
+}
